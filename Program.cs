83634d58312968bbdd65de9d59f70ea7adcd8868
Zachary Renyhart <Zachary.Renyhart@mssa.newhorizons.com>
2024-05-16 15:30:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Zachary_Renyhart_Week_2_Challenge__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the width");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = width; i >= 1; i--) //This is subtracting
+            {
+                for (int j = 1; j <= i; j++) //This adds the counter
+                {
+                    Console.Write(num1);
+                }
+                Console.WriteLine();
+        
+            }   
+        }        
+    }
+}
